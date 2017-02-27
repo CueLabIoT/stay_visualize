d55678189684370da9c37c878e3242c45d91f50a
zashiperm <l05109@shibaura-it.ac.jp>
2017-02-27 18:55:07 +0900

    データ取得のバグ修正

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -1,5 +1,5 @@
 class MeetingController < ApplicationController
   def show
-    @latest_room_status = Meeting.select("room_floor,room_name,flag,used_time,MAX(datetime) as datetime");
+    @latest_room_status = Meeting.group(:room_floor,:room_name).select("room_floor,room_name,flag,used_time,MAX(datetime) as datetime");
   end
 end
